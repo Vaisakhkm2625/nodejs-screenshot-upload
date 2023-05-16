f83d904c0bd433bedc74c1bd2968f9084e2e09ae
Vaisakh K M <vaisakhkm2625@gmail.com>
2023-05-16 22:42:59 +0530

    added png

--- server.js
+++ server.js
@@ -5,6 +5,18 @@ const upload = multer({dest: __dirname + '/uploads/images'});
 const app = express();
 const PORT = 3000;
 
+var storage = multer.diskStorage(
+    {
+        destination: './uploads/images',
+        filename: function ( req, file, cb ) {
+            //req.body is empty...
+            //How could I get the new_file_name property sent from client here?
+            cb( null, file.originalname+ '-' + Date.now()+".png");
+        }
+    }
+);
+
+
 app.use(express.static('public'));
 
 app.post('/upload', upload.single('photo'), (req, res) => {
